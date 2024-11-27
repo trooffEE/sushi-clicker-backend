52fa35f293bf7cd1ec8800518fd8f103ad282fd6
Spitsyn Vadim <spicyn.v2001@gmail.com>
2024-11-27 19:57:30 +0700

    feat: added basic graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
 	_ "database/sql"
 	_ "github.com/lib/pq"
 	"github.com/trooffEE/sushi-clicker-backend/internal/app"
 	_ "github.com/trooffEE/sushi-clicker-backend/internal/config"
 	"github.com/trooffEE/sushi-clicker-backend/internal/db"
+	"os"
+	"os/signal"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
 	database := db.NewDatabaseClient()
-	app.InitServer(database)
+	app.InitServer(ctx, database)
+
+	<-ctx.Done()
 }
--- internal/app/server.go
+++ internal/app/server.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"context"
+	"fmt"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
@@ -17,9 +19,10 @@ import (
 type Server struct {
 	Router *mux.Router
 	DB     *sqlx.DB
+	server *http.Server
 }
 
-func InitServer(db *sqlx.DB) {
+func InitServer(ctx context.Context, db *sqlx.DB) {
 	server := &Server{
 		Router: mux.NewRouter(),
 		DB:     db,
@@ -27,8 +30,26 @@ func InitServer(db *sqlx.DB) {
 
 	server.MountMiddlewares()
 	server.MountHandlers()
+	go server.Start(ctx)
 
-	server.Start()
+	select {
+	case <-ctx.Done():
+		server.ShutdownHTTPServer()
+	}
+}
+
+func (s *Server) ShutdownHTTPServer() {
+	fmt.Println("Server shutting down... ⚒")
+	err := s.server.Shutdown(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = s.DB.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Server is down 🫂")
 }
 
 func (s *Server) MountMiddlewares() {
@@ -49,11 +70,11 @@ func (s *Server) MountHandlers() {
 	s.Router.HandleFunc("/api/private/test", hUser.Test).Methods("GET")
 }
 
-func (s *Server) Start() {
+func (s *Server) Start(ctx context.Context) {
 	srv := &http.Server{
 		Handler: handlers.CORS(
 			handlers.AllowCredentials(),
-			handlers.AllowedOrigins([]string{"http://localhost:5173"}), // TODO local development, later will add normal domain
+			handlers.AllowedOrigins([]string{"http://localhost:5173"}), // TODO local development, later will add normal origin
 			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
 			handlers.AllowedHeaders([]string{"Authorization", "Content-Type", "X-Requested-With"}),
 		)(s.Router),
@@ -61,6 +82,8 @@ func (s *Server) Start() {
 		WriteTimeout: 10 * time.Second,
 		ReadTimeout:  10 * time.Second,
 	}
+
+	s.server = srv
 
 	if err := srv.ListenAndServe(); err != nil {
 		log.Println(err)
